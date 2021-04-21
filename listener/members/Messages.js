9c85960216d01c8dd6af16f4fb4addb57bfe09c6
Gaëtan H <steellgold0@gmail.com>
2021-04-21 23:59:05 +0200

    Anti Spam

--- listener/members/Messages.js
+++ listener/members/Messages.js
@@ -1,11 +1,15 @@
 const { Client, Colors, Config, MySQL, Connection, Game, Time,Embed} = require("../../Stonkser");
 const Registry = require('../../managers/Registry')
 
+const COOLDOWN = []
+
 Client.on("message", message => {
     if (!message.guild) return;
     if (message.author.bot) return;
 
-    Connection.query(`SELECT * FROM guilds WHERE guild_id = ${message.guild.id}`, function (error, guildDB, fields) {
+    Connection.query(`SELECT *
+                      FROM guilds
+                      WHERE guild_id = ${message.guild.id}`, function (error, guildDB, fields) {
         if (error) throw error;
 
         const Prefix = guildDB[0].bot_prefix;
@@ -17,59 +21,78 @@ Client.on("message", message => {
 
         // TODO: refaire l'orga
         if (cmd) {
-            const LANG = require('../../resources/langs/' + guildDB[0].bot_lang)
-            const JOBS = require('../../resources/langs/jobs/Job_' + guildDB[0].bot_lang + '.json')
-            const TOWNS = require('../../resources/Towns.json')
 
-            Connection.query(`SELECT * FROM users WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`, function (error, results) {
-                if (error) throw error
-                if(!Registry.SleepCommands.includes(command)) cmd.run(Client, message, args, results[0], JOBS, TOWNS, LANG, Prefix)
-                if(results[0].sleeping_time === null){
-                    cmd.run(Client, message, args, results[0], JOBS, TOWNS, LANG, Prefix)
-                }else{
-                    Time.getTime(false,null,true).then(value => {
-                        if(value >= results[0].sleeping_time){
-                            Connection.query(`UPDATE users SET ? WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`, { sleeping_time: null });
-                            return Embed.send(message.channel,message,Embed.SLEEP,LANG.translate("SLEEP_STOP"))
-                        }else{
-                            message.channel.send(":sleeping_accommodation: :zzz:").then(msg => {
-                                msg.delete({ timeout: 2000 })
-                                message.delete()
-                            })
-                        }
-                    })
-                }
-            });
+            if(!COOLDOWN[message.author.id]) COOLDOWN[message.author.id] = Date.now() + 3000
+            if (Date.now() >= COOLDOWN[message.author.id]) {
+                COOLDOWN[message.author.id] = Date.now() + 3000
+                const LANG = require('../../resources/langs/' + guildDB[0].bot_lang)
+                const JOBS = require('../../resources/langs/jobs/Job_' + guildDB[0].bot_lang + '.json')
+                const TOWNS = require('../../resources/Towns.json')
 
-            /**
-             Connection.query(`SELECT * FROM prison WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`, function (error, tracjetsResults) {
-                if (error) throw error
-                if (tracjetsResults.length > 0) {
-                    if(tracjetsResults[0].expiration >= Time.getTime(false)){
-                        Connection.query(`DELETE FROM prison WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`);
-                        return message.reply(LANG.translate("PRISON_EXIT")).then(msg => {
-                            msg.delete({timeout: 10000});
+                Connection.query(`SELECT *
+                                  FROM users
+                                  WHERE user_id = ${message.author.id}
+                                    AND guild_id = ${message.guild.id}`, function (error, results) {
+                    if (error) throw error
+                    if (results[0].sleeping_time !== null) {
+                        if (!Registry.SleepCommands.includes(command)) return cmd.run(Client, message, args, results[0], JOBS, TOWNS, LANG, Prefix)
+                        Time.getTime(false, null, true).then(value => {
+                            if (value >= results[0].sleeping_time) {
+                                Connection.query(`UPDATE users
+                                                  SET ?
+                                                  WHERE user_id = ${message.author.id}
+                                                    AND guild_id = ${message.guild.id}`, {sleeping_time: null});
+                                return Embed.send(message.channel, message, Embed.SLEEP, LANG.translate("SLEEP_STOP"))
+                            } else {
+                                message.channel.send(":sleeping_accommodation: :zzz:").then(msg => {
+                                    msg.delete({timeout: 2000})
+                                    message.delete()
+                                })
+                            }
                         })
-                    }else{
-                        return message.reply(LANG.translate("PRISON_ERROR_IN")).then(msg => {
-                            msg.delete({timeout: 10000});
+                    } else if (results[0].prison_time !== null) {
+                        if (!Registry.PrisonCommands.includes(command)) return cmd.run(Client, message, args, results[0], JOBS, TOWNS, LANG, Prefix)
+                        Time.getTime(false, null, true).then(value => {
+                            if (value >= results[0].prison_time) {
+                                Connection.query(`UPDATE users
+                                                  SET ?
+                                                  WHERE user_id = ${message.author.id}
+                                                    AND guild_id = ${message.guild.id}`, {prison_time: null});
+                                return Embed.send(message.channel, message, Embed.PRISON, LANG.translate("PRISON_EXIT"))
+                            } else {
+                                return Embed.send(message.channel, message, Embed.PRISON, LANG.translate("PRISON_ERROR_IN"), {
+                                    0: {
+                                        "title": LANG.translate("PRISON_ERROR_IN_0"),
+                                        "content": LANG.translate("PRISON_ERROR_IN_1") +
+                                            LANG.translate("PRISON_ERROR_IN_2") +
+                                            LANG.translate("PRISON_ERROR_IN_3"),
+                                        "inline": false
+                                    },
+                                    1: {
+                                        "title": LANG.translate("PRISON_ERROR_IN_4"),
+                                        "content": LANG.translate("PRISON_ERROR_IN_5"),
+                                        "inline": false
+                                    },
+                                    2: {
+                                        "title": LANG.translate("PRISON_ERROR_IN_6"),
+                                        "content": LANG.translate("PRISON_ERROR_IN_7", Prefix),
+                                        "inline": false
+                                    }
+                                })
+                            }
                         })
+                    } else {
+                        cmd.run(Client, message, args, results[0], JOBS, TOWNS, LANG, Prefix)
                     }
-                } else {
-                    Connection.query(`SELECT * FROM users WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`, function (error, results) {
-                        if (error) throw error
-                        if (results.length > 0) {
-                            Connection.query(`SELECT * FROM users WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`, function (error, results) {
-                                if (error) throw error;
-
-                            });
-                        } else {
-                            Game.createUser(message.author.id, message.guild.id)
-                        }
-                    });
-                }
-            });
-             */
+                });
+            }else{
+                message.reply("Please, wait 3 seconds").then(msg => {
+                    msg.delete({
+                        timeout: 2000
+                    })
+                    message.delete()
+                })
+            }
         }
     });
 });
