5c7d14b1d01b91e9b36f60da9fb6f7cb8f18ffcc
Gaëtan H <steellgold0@gmail.com>
2021-04-21 23:59:25 +0200

    Ajout du gain d'argent au travail

--- commands/job/Work.js
+++ commands/job/Work.js
@@ -21,14 +21,12 @@ module.exports.run = async (Client, message, args, USER, JOBS, TOWN, LANG, PREFI
             addMoney(message.author.id,message.guild.id,EARNED_CLOWN,USER)
 
             return Embed.send(message.channel, message, Embed.WORK,LANG.translate("WORK_SUCCESS_CLOWN", BIRTHDAYS, EARNED_CLOWN))
-        break;
         case "plumber":
             const REPAIRS = Math.round(getRandomInt(12 / 2))
             const EARNED_PLUMBER = REPAIRS * 60
             addFatigue(message.author.id, message.guild.id, REPAIRS, USER)
-            addMoney(message.author.id,message.guild.id, REPAIRS,USER)
+            addMoney(message.author.id,message.guild.id, EARNED_PLUMBER,USER)
             return Embed.send(message.channel, message, Embed.WORK,LANG.translate("WORK_SUCCESS_PLUMBER", REPAIRS, EARNED_PLUMBER))
-        break;
     }
 }
 
@@ -37,7 +35,7 @@ function addFatigue(member, guild, count, USER){
 }
 
 function addMoney(member, guild, count, USER){
-    Connection.query(`UPDATE users SET ? WHERE user_id = ${member} AND guild_id = ${guild}`, {money: parseInt(USER.money) + count});
+    Connection.query(`UPDATE users SET ? WHERE user_id = ${member} AND guild_id = ${guild}`, {money_bank: parseInt(USER.money_bank) + count});
 }
 
 function getRandomInt(max) {
