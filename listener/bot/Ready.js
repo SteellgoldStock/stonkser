fa467056531d08822482b5be3b9d9e145f18815e
Gaëtan H <steellgold0@gmail.com>
2021-04-22 00:01:20 +0200

    Commandes

--- listener/bot/Ready.js
+++ listener/bot/Ready.js
@@ -15,13 +15,16 @@ Client.on("ready", () => {
     // JEU
     Registry.registerCommand(Client.commands, "rob", false, false, "commands/rp/", "Rob", Colors, true, true);
     Registry.registerCommand(Client.commands, "job", false, false, "commands/job/", "Job", Colors, true, true);
-    Registry.registerCommand(Client.commands, "account", false, false, "commands/user/", "Account", Colors, true, true);
+    Registry.registerCommand(Client.commands, "account", false, false, "commands/user/", "Account", Colors, false, true);
     Registry.registerCommand(Client.commands, "bank", false, false, "commands/user/", "Bank", Colors, true, true);
     Registry.registerCommand(Client.commands, "plane", false, false, "commands/rp/", "Plane", Colors, true, true);
     Registry.registerCommand(Client.commands, "pay", false, false, "commands/user/", "Pay", Colors, true, true);
     Registry.registerCommand(Client.commands, "work", false, false, "commands/job/", "Work", Colors, true, true);
     Registry.registerCommand(Client.commands, "sleep", false, false, "commands/user/", "Sleep", Colors, true, true);
     Registry.registerCommand(Client.commands, "rent", false, false, "commands/rp/", "Rent", Colors, true, true);
+    Registry.registerCommand(Client.commands, "cards", false, false, "commands/rp/", "Cards", Colors,false, true);
+    Registry.registerCommand(Client.commands, "mafia", false, false, "commands/rp/", "Mafia", Colors,false, true);
+    Registry.registerCommand(Client.commands, "escape", false, false, "commands/rp/", "Escape", Colors,false, true);
     //Registry.registerCommand(Client.commands, "town", "", [], false, false, "commands/user/", "Town")
 
     // BOT
