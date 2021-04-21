fdad10d85f18b3a7c05d925ec5a7ba6c270b4bdb
Gaëtan H <steellgold0@gmail.com>
2021-04-21 20:04:36 +0200

    Ajout du temp d'attente sommeil (WORK)

--- commands/user/Sleep.js
+++ commands/user/Sleep.js
@@ -1,16 +1,20 @@
 const Discord = require("discord.js");
-const { Client, Colors, Version, Embed, Connection, Game} = require("../../Stonkser");
-const Time = require('../../managers/Time')
+const { Client, Colors, Version, Embed, Connection, Game, Time} = require("../../Stonkser");
 
 module.exports.run = async (Client, message, args, USER, JOBS, TOWN, LANG, PREFIX) => {
     if (!message.guild) return;
 
     if(!USER.fatigue >= 50) return Embed.send(message.channel,message,Embed.SLEEP,LANG.translate("WORK_DONT_FATIGUE"), null, false, Embed.ERROR_COLOR)
-    delFatigue(message.author.id,message.guild.id)
 
-    // TODO: Cooldown hours
-    await Embed.send(message.channel, message, Embed.SLEEP, LANG.translate("SLEEP_START"))
-    return Embed.send(message.channel,message,Embed.SLEEP,LANG.translate("SLEEP_STOP"))
+    Time.getTime(true, {
+        hours: 0,
+        minutes: 1,
+        seconds: 0
+    },true).then(value => {
+        delFatigue(message.author.id,message.guild.id)
+        Connection.query(`UPDATE users SET ? WHERE user_id = ${message.author.id} AND guild_id = ${message.guild.id}`, { sleeping_time: value });
+        return Embed.send(message.channel, message, Embed.SLEEP, LANG.translate("SLEEP_START"))
+    })
 }
 
 function delFatigue(member, guild){
--- managers/Registry.js
+++ managers/Registry.js
@@ -1,12 +1,13 @@
 const { } = require('../Stonkser')
 exports.PrisonCommands = []
+exports.SleepCommands = []
 
-exports.registerCommand = async(commands, name = "default", isExperimental = false, onlyTestServer = true, directory = "commands/", file = "default", Colors, isAffectedByPrison = false) => {
+exports.registerCommand = async(commands, name = "default", isExperimental = false, onlyTestServer = true, directory = "commands/", file = "default", Colors, isAffectedByPrison = false, isAffectedBySleep = false) => {
     let props = require('../' + directory + file + ".js")
     commands.set(name, props)
 
     if(isAffectedByPrison) exports.PrisonCommands.push(name)
-    console.log(exports.PrisonCommands)
+    if(isAffectedBySleep) exports.SleepCommands.push(name)
 
     console.log(Colors.yellow(`Commande enregistré:`) + Colors.green(`\n- Nom: ${name}\n- Répertoire: ${directory}${file}.js`))
 }
