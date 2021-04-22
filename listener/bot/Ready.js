49e5f6239d6319a5dedd8a4e18c4971f60a0f1a3
Gaëtan H <steellgold0@gmail.com>
2021-04-23 01:11:13 +0200

    Ajout du paramètre "isGameCommand"

--- listener/bot/Ready.js
+++ listener/bot/Ready.js
@@ -13,25 +13,26 @@ Client.on("ready", () => {
     });
 
     // JEU
-    Registry.registerCommand(Client.commands, "rob", false, false, "commands/rp/", "Rob", Colors, true, true);
-    Registry.registerCommand(Client.commands, "job", false, false, "commands/job/", "Job", Colors, true, true);
-    Registry.registerCommand(Client.commands, "account", false, false, "commands/user/", "Account", Colors, false, true);
-    Registry.registerCommand(Client.commands, "bank", false, false, "commands/user/", "Bank", Colors, true, true);
-    Registry.registerCommand(Client.commands, "plane", false, false, "commands/rp/", "Plane", Colors, true, true);
-    Registry.registerCommand(Client.commands, "pay", false, false, "commands/user/", "Pay", Colors, true, true);
-    Registry.registerCommand(Client.commands, "work", false, false, "commands/job/", "Work", Colors, true, true);
-    Registry.registerCommand(Client.commands, "sleep", false, false, "commands/user/", "Sleep", Colors, true, true);
-    Registry.registerCommand(Client.commands, "rent", false, false, "commands/rp/", "Rent", Colors, true, true);
-    Registry.registerCommand(Client.commands, "cards", false, false, "commands/rp/", "Cards", Colors,false, true);
-    Registry.registerCommand(Client.commands, "mafia", false, false, "commands/rp/", "Mafia", Colors,false, true);
-    Registry.registerCommand(Client.commands, "escape", false, false, "commands/rp/", "Escape", Colors,false, true);
+    Registry.registerCommand(Client.commands, "rob", false, false, "commands/rp/", "Rob", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "job", false, false, "commands/job/", "Job", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "account", false, false, "commands/user/", "Account", Colors, false, true, true);
+    Registry.registerCommand(Client.commands, "bank", false, false, "commands/user/", "Bank", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "plane", false, false, "commands/rp/", "Plane", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "pay", false, false, "commands/user/", "Pay", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "work", false, false, "commands/job/", "Work", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "sleep", false, false, "commands/user/", "Sleep", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "rent", false, false, "commands/rp/", "Rent", Colors, true, true, true);
+    Registry.registerCommand(Client.commands, "cards", false, false, "commands/rp/", "Cards", Colors,false, true, true);
+    Registry.registerCommand(Client.commands, "mafia", false, false, "commands/rp/", "Mafia", Colors,false, true, true);
+    Registry.registerCommand(Client.commands, "escape", false, false, "commands/rp/", "Escape", Colors,false, true, true);
     //Registry.registerCommand(Client.commands, "town", "", [], false, false, "commands/user/", "Town")
 
     // BOT
-    Registry.registerCommand(Client.commands, "about", false, false, "commands/", "About", Colors)
-    Registry.registerCommand(Client.commands, "help", false, false, "commands/", "Help", Colors)
+    Registry.registerCommand(Client.commands, "about", false, false, "commands/", "About", Colors, false, false, false)
+    Registry.registerCommand(Client.commands, "help", false, false, "commands/", "Help", Colors, false, false, false)
 
     // CONFIGURATIONS
-    Registry.registerCommand(Client.commands, "prefix", false, false,"commands/config/", "Prefix", Colors)
-    Registry.registerCommand(Client.commands, "lang", false, false,"commands/config/", "Lang", Colors)
+    Registry.registerCommand(Client.commands, "channels", false, false, "commands/config/", "Channels", Colors, false, false, false)
+    Registry.registerCommand(Client.commands, "prefix", false, false,"commands/config/", "Prefix", Colors, false, false, false)
+    Registry.registerCommand(Client.commands, "lang", false, false,"commands/config/", "Lang", Colors, false, false, false)
 });
